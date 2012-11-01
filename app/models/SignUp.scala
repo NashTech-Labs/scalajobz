72dc2a44456621c1b89c58b2686373505ab162a1
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-01 23:13:26 +0530

    Mongo DB Setup

--- app/models/SignUp.scala
+++ app/models/SignUp.scala
@@ -1,12 +1,23 @@
 package models
 
+import com.novus.salat.global._
+import com.novus.salat.annotations._
+import org.bson.types.ObjectId
+import com.novus.salat.dao.SalatDAO
+import utils.MongoHQConfig
 
 case class SignUpForm(emailId: String, password: String,confirmPassword:String)
-
+case class User(@Key("_id") id: ObjectId,emailId:String,password:String)
 object SignUp {
 
-  def createUser(emailId: String,password:String) {
-    println(emailId,password)
+  /**
+ * Create New User 
+ */
+  def createUser(emailId: String,password:String)= {
+    val newUser=User(new ObjectId,emailId,password)
+    UserDAO.insert(newUser)
   }
-  
 }
+
+
+object UserDAO extends SalatDAO[User, ObjectId](collection = MongoHQConfig.mongoDB("user"))
