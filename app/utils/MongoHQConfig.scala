0364ad60edb487ba4256233cf9be73ac2441a078
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-01 23:46:33 +0530

    MongoHq Setup

---
+++ app/utils/MongoHQConfig.scala
@@ -0,0 +1,12 @@
+package utils
+import com.mongodb.casbah.MongoConnection
+
+object MongoHQConfig {
+
+//  val mongoDB = MongoConnection("", 27017)("scalajobz")
+//  mongoDB.authenticate("", "")
+
+  val mongoDB = MongoConnection("alex.mongohq.com", 10083)("scalajobz")
+  mongoDB.authenticate("neel", "neel")
+
+}
