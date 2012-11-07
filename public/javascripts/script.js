9c83923a13d7ec89dd98efe1cd02463d3e608a7e
Ruchi Jindal (ruchi@knoldus.com) <ruchi@knoldus.com>
2012-11-07 15:53:45 +0530

    MongoHQ Settings

---
+++ public/javascripts/script.js
@@ -0,0 +1,98 @@
+// 
+//	jQuery Validate example script
+//
+//	Prepared by David Cochran
+//	
+//	Free for your use -- No warranties, no guarantees!
+//
+
+$(document).ready(
+		function() {
+			// Validate
+			// http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+			// http://docs.jquery.com/Plugins/Validation/
+			// http://docs.jquery.com/Plugins/Validation/validate#toptions
+
+			$('#login-form').validate(
+					{
+						rules : {
+							Password : {
+								minlength : 6,
+								required : true
+							},
+							EmailId : {
+								required : true,
+								email : true
+							}
+						},
+						highlight : function(label) {
+							$(label).closest('.control-group')
+									.addClass('error');
+						},
+						success : function(label) {
+							label.text('OK!').addClass('valid').closest(
+									'.control-group').addClass('success');
+						}
+					});
+
+			$('#signup-form').validate(
+					{
+						rules : {
+							Password : {
+								minlength : 6,
+								required : true
+							},
+							ConfirmPassword : {
+								equalTo : "#Password",
+								required : true
+							},
+							EmailId : {
+								required : true,
+								email : true
+							}
+						},
+						highlight : function(label) {
+							$(label).closest('.control-group')
+									.addClass('error');
+						},
+						success : function(label) {
+							label.text('OK!').addClass('valid').closest(
+									'.control-group').addClass('success');
+						}
+					});
+
+			$('#jobpost-form').validate(
+					{
+						rules : {
+							Position : {
+								minlength : 2,
+								required : true
+							},
+							Company : {
+								minlength : 2,
+								required : true
+							},
+							Location : {
+								minlength : 2,
+								required : true
+							},
+							Email_Addrss_To_Apply_To : {
+								required : true,
+								email : true
+							},
+							Description : {
+								minlength : 2,
+								required : true
+							},
+						},
+						highlight : function(label) {
+							$(label).closest('.control-group')
+									.addClass('error');
+						},
+						success : function(label) {
+							label.text('OK!').addClass('valid').closest(
+									'.control-group').addClass('success');
+						}
+					});
+
+		}); // end document.ready
