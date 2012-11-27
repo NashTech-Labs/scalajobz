6248c6ab3f5e79fae7df39b5983ccf5672dc4e1c
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-27 16:43:58 +0530

    Application Controller Test

--- test/controllers/ApplicationTest.scala
+++ test/controllers/ApplicationTest.scala
@@ -9,9 +9,42 @@ import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ApplicationTest extends Specification {
-  "Hello" in {
+  "index" in {
     val result = controllers.Application.index(FakeRequest())
     status(result) must equalTo(OK)
-     contentType(result) must beSome("text/html")
+    contentType(result) must beSome("text/html")
+  }
+
+  "signUpOnScalaJobz" in {
+    val result = controllers.Application.signUpOnScalaJobz("login")(FakeRequest())
+    status(result) must equalTo(OK)
+    contentType(result) must beSome("text/html")
+    contentAsString(result) must contain("login")
+  }
+
+  "newUser" in {
+    val result = controllers.Application.newUser("login")(FakeRequest())
+    contentType(result) must beSome("text/html")
+    contentAsString(result) must contain("login")
+    status(result) must equalTo(400)
+  }
+
+  "logIn" in {
+    val result = controllers.Application.logIn("login")(FakeRequest())
+    contentType(result) must beSome("text/html")
+    contentAsString(result) must contain("login")
+    status(result) must equalTo(400)
+  }
+
+  "loginOnScalaJobz" in {
+    val result = controllers.Application.loginOnScalaJobz(FakeRequest())
+    status(result) must equalTo(OK)
+    contentType(result) must beSome("text/html")
+  }
+
+  "logOutFromScalaJobz" in {
+    val result = controllers.Application.logOutFromScalaJobz(FakeRequest())
+    status(result) must equalTo(OK)
+    contentType(result) must beSome("text/html")
   }
 }
