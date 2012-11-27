bce34f57c7240be6b3f770795d921c953bb3c08a
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-27 10:28:43 +0530

    Controller Testing

---
+++ test/controllers/ApplicationTest.scala
@@ -0,0 +1,9 @@
+//package controllers
+//import models.Job
+//
+//class ApplicationTest {
+//"" in {
+//  val result = controllers.Application.index(FakeRequest())
+//  status(result) must equalTo(OK)
+//}
+//}
