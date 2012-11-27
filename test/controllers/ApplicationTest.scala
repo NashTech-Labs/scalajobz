0220a3415c5da84dcee20ee6655cd6aec59e1e90
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-27 16:27:06 +0530

    Org.spec2 integrated

--- test/controllers/ApplicationTest.scala
+++ test/controllers/ApplicationTest.scala
@@ -1,9 +1,17 @@
-//package controllers
-//import models.Job
-//
-//class ApplicationTest {
-//"" in {
-//  val result = controllers.Application.index(FakeRequest())
-//  status(result) must equalTo(OK)
-//}
-//}
+package controllers
+
+import models.Job
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ApplicationTest extends Specification {
+  "Hello" in {
+    val result = controllers.Application.index(FakeRequest())
+    status(result) must equalTo(OK)
+     contentType(result) must beSome("text/html")
+  }
+}
