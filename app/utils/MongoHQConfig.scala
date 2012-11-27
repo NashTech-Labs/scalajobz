b86dc3e2fb5b459d556ab3932f68c8795740ef73
Ruchi Jindal (ruchi@knoldus.com) <ruchi@knoldus.com>
2012-11-27 15:51:36 +0530

    Update MongoHq

--- app/utils/MongoHQConfig.scala
+++ app/utils/MongoHQConfig.scala
@@ -1,10 +1,17 @@
 package utils
 import com.mongodb.casbah.MongoConnection
+import play.api.Play
 
 object MongoHQConfig {
 
-  val mongoDB = MongoConnection("", 27017)("scalajobz")
-  mongoDB.authenticate("", "")
+  val url = Play.current.configuration.getString("url").get.toString
+  val port = Play.current.configuration.getString("port").get.toInt
+  val db = Play.current.configuration.getString("dbname").get.toString
+  val username = Play.current.configuration.getString("username").get.toString
+  val password = Play.current.configuration.getString("password").get.toString
+
+  val mongoDB = MongoConnection(url, port)(db)
+  mongoDB.authenticate(username, password)
 
   // val mongoDB = MongoConnection("alex.mongohq.com", 10083)("scalajobz")
   // mongoDB.authenticate("neel", "neel")
