05ad64342f49b804c3576ed351b3c4c188a6fbdc
Neelkanth Sachdeva <neelkanth@knoldus.com>
2012-11-27 10:10:26 +0530

    Update user

--- test/models/UserTest.scala
+++ test/models/UserTest.scala
@@ -16,17 +16,16 @@ class UserTest extends FunSuite with BeforeAndAfter {
     val employers = User.findUserByEmail("neelkanth@knoldus.com")
     assert(employers.size === 1)
   }
-  
-//  test("Update Employer"){
-//     val employer = Employer(new ObjectId, "neelkanth@knoldus.com", "ABCD", List(), false)
-//    val employerId = User.createUser(employer)
-//    val employers = User.findUserByEmail("neelkanth@knoldus.com")
-//    assert(employers.head.jobSeeker===false)
-//    val employerWithChangedDetails=Employer(new ObjectId, "neelkanth@knoldus.com", "ABCDEF", List(), true)
-//    val anotherEmployer=User.updateUser(employerWithChangedDetails,"")
-//    assert(User.findUserById(employerId.get.toString).get.password==="ABCDEF")
-//  }
-   after {
+
+  test("Update Employer") {
+    val employer = Employer(new ObjectId, "neelkanth@knoldus.com", "ABCD", List(), false)
+    User.createUser(employer)
+    val employers = User.findUserByEmail("neelkanth@knoldus.com")
+    assert(employers.head.jobSeeker === false)
+    val updatedEmployer = User.updateUser(employer, "ABCDEF")
+    assert(User.findUserById(employer.id.toString).get.password === "ABCDEF")
+  }
+  after {
     EmployerDAO.remove(MongoDBObject("emailId" -> ".*".r))
   }
 
