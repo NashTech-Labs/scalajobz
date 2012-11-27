543a0452c124e603b85f55e8cc0870836c37bcb2
Ruchi Jindal (ruchi@knoldus.com) <ruchi@knoldus.com>
2012-11-27 17:38:19 +0530

    Add Test For Template

---
+++ test/template/TemplateTest.scala
@@ -0,0 +1,27 @@
+package template
+
+import models.Job
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import models.Alert
+
+@RunWith(classOf[JUnitRunner])
+class TemplateTest extends Specification {
+
+  "index template" in {
+    val html = views.html.index(new Alert("Welcome", "Welcome in scalajobz"), null, List(), false)
+    println("html")
+    contentType(html) must equalTo("text/html")
+    contentAsString(html) must contain("Welcome")
+  }
+  
+  "ajax result template" in {
+    val html = views.html.ajax_result(List(), false)
+    println("html")
+    contentType(html) must equalTo("text/html")
+  }
+
+}
