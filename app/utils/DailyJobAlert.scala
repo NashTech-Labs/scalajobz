1a0a8e0e308f49d32c1e66d07a3068b3762bf91b
Ruchi Jindal (ruchi@knoldus.com) <ruchi@knoldus.com>
2012-11-28 16:13:08 +0530

    Update

--- app/utils/DailyJobAlert.scala
+++ app/utils/DailyJobAlert.scala
@@ -15,6 +15,7 @@ object DailyJobAlert {
    * Send Mail To Job Seekers on the basis of their search criteria Via  Akka Actor
    */
   def sendMailIForJobAlert: Unit = {
+    println("hiiiiiiiiiii")
     val system = ActorSystem("jobActors")
     val jobActor = system.actorOf(Props[JobAlertActor])
     val jobSeekers = User.findJobSeekers
--- app/utils/JobAlertActor.scala
+++ app/utils/JobAlertActor.scala
@@ -12,7 +12,7 @@ class JobAlertActor extends Actor {
   def receive : PartialFunction[Any,Unit] = {
     case jobAlertMail: JobAlertMail =>
       SendEmail.sendEmail(jobAlertMail.jobSeeker, jobAlertMail.jobs)
-      context.system.scheduler.scheduleOnce(1 hours, self, JobAlertMail(jobAlertMail.jobSeeker, jobAlertMail.jobs))
+      context.system.scheduler.scheduleOnce(30 seconds, self, JobAlertMail(jobAlertMail.jobSeeker, jobAlertMail.jobs))
   }
 }
 
